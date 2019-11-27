32f4deb02008b4a765740106814d022269f2a9cb
Joel Noyce Barnham <joelnbarnham@gmail.com>
2019-11-27 21:39:12 +0000

    Improve test template

--- tests/test_winpv.py
+++ tests/test_winpv.py
@@ -7,22 +7,40 @@ from win_pyxs import XenBusConnectionWinPV
 
 class WinPVTester(unittest.TestCase):
 
-    @mock.patch('wmi.WMI')
-    def test_connect(self, wmi_mock):
-        base_mock = mock.MagicMock(name='wmi.WMI.XenProjectXenStoreBase')
-        base_mock.AddSession.return_value = [3]
+    def setUp(self):
+        self.base_mock = mock.MagicMock(name='wmi.WMI.XenProjectXenStoreBase')
+        self.base_mock.AddSession.return_value = [3]
 
-        session_mock = mock.MagicMock(
+        self.session_mock = mock.MagicMock(
             name='wmi.WMI.XenProjectXenStoreBase.AddSession'
         )
 
-        wmi_mock.return_value.XenProjectXenStoreBase.return_value = [base_mock]
-        wmi_mock.return_value.query.return_value = [session_mock]
+        self.connection_params = [{}, {'xs_session_name': 'CUSTOM_session'}]
+        self.connections = []
+        for kwargs in self.connection_params:
+            self.connections.append(XenBusConnectionWinPV(**kwargs))
 
-        connection = XenBusConnectionWinPV()
-        connection.connect()
+        self.wmi_mock = mock.MagicMock(name='wmi.WMI')
+        self.wmi_mock.return_value.XenProjectXenStoreBase.return_value = [
+            self.base_mock
+        ]
+        self.wmi_mock.return_value.query.return_value = [self.session_mock]
 
-        wmi_mock.assert_called_with(moniker="//./root/wmi", find_classes=False)
-        base_mock.AddSession.assert_called_with(Id="PyxsSession")
+    def test_connect(self):
+        with mock.patch('wmi.WMI', new=self.wmi_mock) as wmi_mock:
+            for idx, connection in enumerate(self.connections):
+                connection.connect()
 
-        self.assertEqual(connection.session, session_mock)
+                wmi_mock.assert_called_with(
+                    moniker="//./root/wmi", find_classes=False
+                )
+                self.base_mock.AddSession.assert_called_with(
+                    Id=self.connection_params[idx].
+                    get('xs_session_name', 'PyxsSession')
+                )
+                self.assertEqual(connection.session, self.session_mock)
+
+                connection.close()
+
+                self.session_mock.EndSession.assert_called_with()
+                self.assertEqual(connection.session, None)
