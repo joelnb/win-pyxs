e60aa60ce0c4374a1176f4d7776531e2e6de87b6
Joel Noyce Barnham <joelnbarnham@gmail.com>
2019-11-27 08:09:46 +0000

    Add sphinx documentation generation

---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. win_pyxs documentation master file, created by
+   sphinx-quickstart on Wed Nov 27 08:04:12 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to win_pyxs's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+
+   win_pyxs
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ win_pyxs/utils.py
@@ -0,0 +1,23 @@
+"""
+This module contains utility functions used by win_pyxs which should not be
+needed by any client code & are only intended for internal use.
+"""
+
+
+class LazyVar(object):
+    """
+    Wrap a function in a LazyVar to create a callable whose return value is
+    calculated no moe than once & then stored and returned for future callers.
+    This can be useful to reduce the time taken to initialise expensive
+    values without having to use a global varaible within a module.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self):
+        try:
+            return self.value
+        except AttributeError:
+            self.value = self.func()
+            return self.value
